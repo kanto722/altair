c87a92c40ac12f82d853d954c3175d73a00eb733
Kanto <kanto72@yandex.ru>
2020-12-17 16:35:40 +0500

    ska okna

---
+++ pages/monitor/js/tabs.js
@@ -0,0 +1,19 @@
+
+//car
+const questionsBox = document.querySelector('.car-questions__content');
+const questions = questionsBox.querySelectorAll('.car-questions');
+questionsBox.addEventListener('click', function (e) {
+  const target = e.target;
+  if (!target.classList.contains('car-questions__toggle')) { return; }
+  const question = target.closest('.car-question');
+  if (!question) { return; }
+  if (question.classList.contains('open')) {
+    question.classList.remove('open');
+    console.log('.open');
+    return;
+  }
+  questions.forEach((questionItem) => {
+    questionItem.classList.remove('open');
+  });
+  question.classList.add('open');
+});
