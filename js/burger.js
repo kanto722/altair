cc35cf6064ba5e6b27ad993573f7732fe86ba242
Kanto <kanto72@yandex.ru>
2020-12-30 17:44:14 +0500

    go home

--- js/burger.js
+++ js/burger.js
@@ -78,3 +78,26 @@ function sendFormOne(sendButton, inputName_Name, inputName_Phone) {
 
   //mask input
 $('.number-mask').mask('+7 (999) 999-9999'); 
+
+
+// Отложенная загрузка карты
+var YaMapsShown = false;
+
+$(window).scroll(function () {
+  var offset = $('.content__maps').offset().top;
+  if (!YaMapsShown) {
+    if ($(window).scrollTop() + $(window).height() > offset - 600) {
+      showYaMaps();
+      YaMapsShown = true;
+    }
+  }
+});
+
+function showYaMaps() {
+   var script = document.createElement("script");
+   script.type = "text/javascript";
+   script.src = "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A36a185c1c90b97dc6b21f77694df4fac833d8723c84c8ead2991cfe0a14f0b33&amp;width=100%25&amp;height=577&amp;lang=ru_RU&amp;scroll=true";
+
+   document.getElementById("maps").appendChild(script);
+ };
+// КОНЕЦ Отложенная загрузка карты
--- pages/contact/js/burger.js
+++ pages/contact/js/burger.js
@@ -7,3 +7,25 @@ headerBurger.onclick = function(){
     headerBlock.classList.toggle('active');
     back.classList.toggle('lock');
 }
+
+// // Отложенная загрузка карты
+// var YaMapsShown = false;
+
+// $(window).scroll(function () {
+//   var offset = $('.content__maps').offset().top;
+//   if (!YaMapsShown) {
+//     if ($(window).scrollTop() + $(window).height() > offset - 600) {
+//       showYaMaps();
+//       YaMapsShown = true;
+//     }
+//   }
+// });
+
+// function showYaMaps() {
+//    var script = document.createElement("script");
+//    script.type = "text/javascript";
+//    script.src = "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A36a185c1c90b97dc6b21f77694df4fac833d8723c84c8ead2991cfe0a14f0b33&amp;width=100%25&amp;height=577&amp;lang=ru_RU&amp;scroll=true";
+
+//    document.getElementById("maps").appendChild(script);
+//  };
+// // КОНЕЦ Отложенная загрузка карты
